ba705c26ae798410fcd73cead975360f08b7d2bd
Mykelsown <mykelsown@gmail.com>
2026-07-28 05:40:30 +0100

    changed from newContract(witnesses) to CompliledContract.make in contract/index.ts

--- contracts/index.ts
+++ contracts/index.ts
@@ -1,22 +1,26 @@
+import { CompiledContract } from '@midnight-ntwrk/midnight-js-protocol/compact-js';
 import path from 'node:path';
-import { Contract, ledger } from './managed/dev_profile/contract/index.js';
-import { type Witnesses } from './managed/dev_profile/contract/index.js';
-import * as __compactRuntime from '@midnight-ntwrk/compact-runtime';
 
-export { Contract, ledger };
-export { Tier, RevealPolicy } from './managed/dev_profile/contract/index.js';
+export {
+  Contract,
+  ledger,
+  pureCircuits,
+  type Ledger,
+  type ImpureCircuits,
+  type PureCircuits,
+  Tier,
+  RevealPolicy,
+} from './managed/dev_profile/contract/index.js';
 
-export const zkConfigPath = path.resolve(
-  new URL(import.meta.url).pathname,
-  '../managed/dev_profile',
+import { Contract } from './managed/dev_profile/contract/index.js';
+
+const currentDir = path.resolve(new URL(import.meta.url).pathname, '..');
+export const zkConfigPath = path.resolve(currentDir, 'managed', 'dev_profile');
+
+export const CompiledDevMatchContract = CompiledContract.make(
+  'DevProfileContract',
+  Contract,
+).pipe(
+  CompiledContract.withVacantWitnesses,
+  CompiledContract.withCompiledFileAssets(zkConfigPath),
 );
-
-const witnesses: Witnesses<Record<string, never>> = {
-  localSecretKey: (context: __compactRuntime.WitnessContext<any, any>) => {
-    const sk = new Uint8Array(32);
-    crypto.getRandomValues(sk);
-    return [context.privateState, sk];
-  },
-};
-
-export const CompiledDevMatchContract = new Contract(witnesses);
